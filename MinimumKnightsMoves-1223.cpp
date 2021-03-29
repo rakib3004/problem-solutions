bcada2bd8ed04c7eee5bf23b6b3bcfb4df1915e3
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-03-29 12:47:46 +0600

    solve the problem with team mates

--- MinimumKnightsMoves-1223.cpp
+++ MinimumKnightsMoves-1223.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+int dx[]={-2,-2,2,2,-1,-1,1,1};
+int dy[]={1,-1,-1,1,2,-2,2,-2};
 int main()
 {
 
@@ -10,7 +12,6 @@ int main()
     {
         string a,b;
         cin>>a>>b;
-
 
         int x1,x2,y1,y2;
         x1=a[0]-'a';
@@ -24,9 +25,7 @@ int main()
             continue;
         }
 
-
         int g[8][8]= {0};
-
 
 //        for(int i=0;i<8;i++){
 //            for(int j=0;j<8;j++)
@@ -43,41 +42,22 @@ int main()
 //            cur=q.front();
             int i=q.front().first;
             int j=q.front().second;
+            q.pop();
 
-            for(int a=i-2; a<=i+3; a++)
+            for(int dir=0; dir<8; dir++)
             {
-                if(a==i||a==i-1||a==i+1)
-                    continue;
+                int nx=i+dx[dir];
+                int ny=j+dy[dir];
 
-                    for(int b=j-1; b<j+2; b++)
-                    {
-                        if(b==j)
-                            continue;
+                if(nx>=0 && nx<8 && ny>=0 && ny<8 && g[nx][ny]==0){
 
-                            if((b>=0 &&b<8) && (a>=0 && a<8) && g[a][b]==0 )
-                            {
-
-                                g[a][b]=g[i][j]+1;
-                                q.push(make_pair(a,b));
-                            }
-
-                    }
+                    g[nx][ny]=g[i][j]+1;
+                    q.push({nx,ny});
+                }
 
             }
 
-            q.pop();
-
-
         }
-
-
-        for(int i=0;i<8;i++){
-                cout<<endl;
-
-            for(int j=0;j<8;j++)
-                cout<<g[i][j]<<" ";
-        }
-cout<<endl<<endl;
         cout<<g[x2][y2]<<"\n";
 
     }
