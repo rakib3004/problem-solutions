29a4f9dbcab525d325f0db8d8eae038c488a20ea
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-10 22:37:40 +0600

    review code

--- MaxSubArraySum-1223.cpp
+++ MaxSubArraySum-1223.cpp
@@ -6,45 +6,49 @@ using namespace std;
 
 int main(){
 
-    ll t; cin>>t;
+    ll t,cas=0; scanf("%lld",&t);
     while(t--){
-    ll n; cin>>n;
+    ll n; scanf("%lld",&n);
     ll a[n];
     for(ll i=0;i<n;i++){
-        cin>>a[i];
+        scanf("%lld",&a[i]);
     }
 
     ll s=0,c=0,o=0;
     for(ll i=0;i<n;i++){
-        if(a[i]>=0)s+=a[i];
+        if(a[i]>0)s+=a[i];
         else o++;
     }
     if(o==n){
         s=INT_MIN;
         for(ll i=0;i<n;i++){
+            if(a[i]>s)c=i;
             s=max(s,a[i]);
         }
-        cout<<s<<" "<<1<<endl;
+
+        printf("Case %lld: %lld %lld\n",++cas,s,0);
         continue;
     }
     ll x=0,y=n;
     for(ll i=0;i<n;i++){
-        if(a[i]<0)x=i;
+        if(a[i]<=0)x=i;
         else
             break;
     }
     for(ll i=n-1;i>=0;i--){
-        if(a[i]<0)y=i;
+        if(a[i]<=0)y=i;
         else
             break;
     }
+
     for(ll i=x+1;i<y;i++){
-        if(a[i]<0){c++;
-        if(a[i]==a[i-1])c--;
+        if(a[i]<=0){c++;
+        if(a[i]==0 && ((a[i-1]>=0 && a[i+1]>=0)||(a[i-1]<=0 && a[i+1]<=0)))c--;
+        else if(a[i]<=0 && a[i-1]<=0)c--;
     }
 
         }
-        cout<<s<<" "<<c<<endl;
+       printf("Case %lld: %lld %lld\n",++cas,s,c);
     }
 
     }
