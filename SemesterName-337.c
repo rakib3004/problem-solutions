1f365f144b5a98b2b36c1a4d02a3a9a9d8ecf118
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-05-05 12:52:54 +0600

    write code in semester name 337

---
+++ SemesterName-337.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+
+
+int main()
+{
+
+char semesterName[4][50]={"S1","S2","S2","S4"};
+printf("%s\n",semesterName[1]);
+printf("%s\n",semesterName[2]);
+printf("%s\n",semesterName[3]);
+
+    return 0;
+
+}
