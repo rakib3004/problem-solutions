e5faa74ed4d5a65ce4724167601b838e0e10318f
rakib3004 <bsse1129@iit.du.ac.bd>
2021-11-20 11:49:29 +0600

    test switch case for detect vowel

---
+++ VowelSwitchCase-337.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+    char alp;
+    printf("Please enter an alphabet: ");
+    scanf("%c",&alp);
+    switch(alp)
+    {
+    case 'a':
+        printf("vowel");
+        break ;
+
+        case 'e':
+        printf("vowel");
+        break ;
+
+        case 'i':
+        printf("vowel");
+        break ;
+
+        case 'o':
+        printf("vowel");
+        break ;
+
+        case 'u':
+        printf("vowel");
+        break ;
+
+        case 'A':
+        printf("vowel");
+        break ;
+
+
+        case 'E':
+        printf("vowel");
+        break ;
+
+        case 'I':
+        printf("vowel");
+        break ;
+
+        case 'O':
+        printf("vowel");
+        break ;
+
+        case 'U':
+        printf("vowel");
+        break ;
+
+        default:
+        printf("Consonant");
+        break;
+
+
+    }
+}
