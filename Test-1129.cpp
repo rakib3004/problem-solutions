543b52dbfb4805572ef0add13b0f42947ad18d31
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-22 22:37:06 +0600

    hello world

---
+++ Test-1129.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+
+
+cout<<"Hello World"<<endl;
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        test();
+
+    }
+
+
+    return 0;
+
+}
