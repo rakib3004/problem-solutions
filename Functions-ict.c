18ea230bbd91346ef21a366b27fbad1ab17796da
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-05-17 23:14:22 +0600

    add simple function for demonstration

---
+++ Functions-ict.c
@@ -0,0 +1,162 @@
+
+#include<stdio.h>// header file  ---- scanf, printf
+
+
+
+// input -> eat
+
+// processing   -> digation
+
+// output -> energy
+
+
+
+
+int main() {
+
+ // data type declearation
+int a; // 5, 6, 11
+
+float b; // 5.0, 18.12  or double
+
+char c;  //  r a k i b, p r othom
+
+
+double ---> lf
+
+
+
+int x,y,z;
+
+scanf("%d%d",&x,&y);
+
+
+int road, house, sector;
+
+scanf("%d%d%d",&road,&house, &sector);
+
+
+float gpa;
+float height;
+
+scanf("%f%f%f",&gpa,&height, &weight);
+
+char p,q,r;
+
+scanf("%c%c",&p,&q);
+scanf("%c",&r);
+
+
+
+
+
+
+int x,y,z;
+
+// &x &y
+// %d
+
+
+scanf("%d%d",&x,&y);
+
+
+
+//z = x + y;
+
+//
+//int x,y,z;
+//
+//// &x &y
+//// %d
+//
+//
+//scanf("%d%d",&x,&y);
+//
+//
+//z = x-y;
+
+printf("My name is prothom\n");
+printf("Laccity\n");
+printf("Number of t-shirt I have %d",z);
+
+
+
+// \n ------- new line
+
+int i=1;
+for(i=1;i<=100;i++){
+
+    printf("Hi\n");
+}
+
+int age =18;
+
+if(age>=18){
+    printf("Phone");
+}
+else{
+     printf(" No Phone");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include<stdio.h>  // scanf, printf
+//#include<conio.h>  // getch(); putch();
+//#include<math.h>   // pow, sqrt
+//
+//
+//int main() {
+//
+//    return 0;
+//}
